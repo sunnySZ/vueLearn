6c685e6c9d2e92f2b3d0befa6965bcb942ae5192
sunnySZ <lkp528@163.com>
2017-07-18 23:21:59 +0800

    删除

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,22 +1,24 @@
 <template>
 	<div>
-<mt-tabbar fixed>
-  <mt-tab-item>
-    <img slot="icon" src="../assets/logo.png">
-  <router-link to="/list">列表</router-link>
-  </mt-tab-item>
-  <mt-tab-item>
-    <img slot="icon" src="../assets/logo.png">
-    <router-link to="/infinite">loading</router-link>
-  </mt-tab-item>
-  <mt-tab-item id="发现">
-    <img slot="icon" src="../assets/logo.png">
-    发现
-  </mt-tab-item>
-  <mt-tab-item id="我的">
-    <img slot="icon" src="../assets/logo.png">
-    我的
-  </mt-tab-item>
+<mt-tabbar v-model="selected" fixed>
+  <mt-tab-item id="list"><img slot="icon" src="../assets/logo.png">列表</mt-tab-item>
+  <mt-tab-item id="infinite"><img slot="icon" src="../assets/logo.png">loading</mt-tab-item>
+  <mt-tab-item id="发现"><img slot="icon" src="../assets/logo.png">发现</mt-tab-item>
+  <mt-tab-item id="我的"><img slot="icon" src="../assets/logo.png">我的</mt-tab-item>
 </mt-tabbar>
 	</div>
 </template>
+<script>
+  export default{
+      data(){
+          return{
+              selected:'list'
+          }
+      },
+      watch:{
+          selected(curVal,oldVal){
+              this.$router.push({path:'/'+curVal})
+          }
+      }
+  }
+</script>
